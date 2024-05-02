7cd328d615b24e5c14ad182cd901e8cddac6e7d7
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-05-02 10:29:46 +0700

    menambahkan API Mahasiswa

---
+++ modul10_1302223007/Controllers/MahasiswaController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace modul10_1302223007.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mhs = new List<Mahasiswa>
+        {
+            new Mahasiswa("Muhammad Haulul Azkiyaa", "1302223007", new List<string>{"KPL", "BasDat", "UX"}, 2022),
+            new Mahasiswa("Fauzein Mulya Warman", "1302223127", new List<string>{"KPL", "PBO", "UX"}, 2022),
+            new Mahasiswa("Iksan Oktav Risandy", "1302223042", new List<string>{"KPL", "DasJarKom", "Proting"}, 2022),
+        };
+
+        // GET: api/<MahasiswaController>
+        [HttpGet]
+        public List<Mahasiswa> Get()
+        {
+            return mhs;
+        }
+
+        // GET api/<MahasiswaController>/5
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return mhs[id];
+        }
+
+        // POST api/<MahasiswaController>
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            mhs.Add(value);
+        }
+
+        // DELETE api/<MahasiswaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            mhs.RemoveAt(id);
+        }
+    }
+}
---
+++ modul10_1302223007/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace modul10_1302223007
+{
+    public class Mahasiswa
+    {
+        public string name { set; get; }
+        public string nim { set; get; }
+        public List<string> course { set; get; }
+        public int year { set; get; }
+
+        public Mahasiswa(string name, string nim, List<string> course, int year)
+        {
+            this.name = name;
+            this.nim = nim;
+            this.course = course;
+            this.year = year;
+        }
+    }
+}
